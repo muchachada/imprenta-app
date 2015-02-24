f4771c70c52162e44bfdca6d80782c11d2baac52
cachi87 <facundosegarra@yahoo.com>
2015-02-24 01:09:31 -0300

    Creación de menu oculto

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -1,2 +1,17 @@
-angular.module('App').controller('MainController', ['$scope', function($scope){
-}]);
+angular.module('App')
+.controller('MainController', function($scope, $timeout, $mdSidenav, $log) {
+  $scope.toggleLeft = function() {
+    $mdSidenav('left').toggle()
+                        .then(function(){
+                          $log.debug("toggle LEFT is done");
+                        });
+  };
+})
+.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
+  $scope.close = function() {
+    $mdSidenav('left').close()
+                        .then(function(){
+                          $log.debug("close LEFT is done");
+                        });
+  };
+});
