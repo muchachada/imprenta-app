ad85693b3d51588f2225d15bd34e0ee8620ca0b9
cachi87 <facundosegarra@yahoo.com>
2015-02-23 22:32:39 -0300

    Cambios en NeDB a persistente

--- app/scripts/services/ClientesService.js
+++ app/scripts/services/ClientesService.js
@@ -1,7 +1,9 @@
-var NeDB = require('nedb');
+var NeDB = require('nedb'),
+path = require('path'),
+gui = require('nw.gui');
 
 angular.module('App').factory('ClientesService', function(){
   var service = {};
-  service.db = new NeDB({autoload: true});
+  service.db = new NeDB({filename: path.join(gui.App.dataPath, 'clientes.db'), autoload: true});
   return service;
 })
