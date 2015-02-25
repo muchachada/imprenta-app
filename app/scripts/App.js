ad737cc03ede12dd25314083ab9e63b382d2a6d9
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2015-02-25 00:33:16 -0300

    Some work on leftmenu.

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -1,4 +1,4 @@
-angular.module('App', ['ngMaterial', 'ui.router'])
+angular.module('App', ['ngMaterial', 'ui.router', 'ngMdIcons'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -1,5 +1,17 @@
-angular.module('App').controller('MainController', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  $scope.openLeftMenu = function() {
+angular.module('App').controller('MainController', [
+  '$scope',
+  '$mdSidenav',
+  '$state',
+  function($scope, $mdSidenav, $state){
+
+  $scope.toggleLeftMenu = function() {
    $mdSidenav('left').toggle();
  };
+
+ $scope.goTo = function(to) {
+   $state.go(to);
+   $scope.toggleLeftMenu();
+ }
+
+
 }]);
